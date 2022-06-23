5308d30293fe2595190725807f39cf3e04977955
Akshay <20bcs59@nith.ac.in>
2022-06-23 16:56:03 +0530

    a

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import './App.css';
 import Navbar from './components/Navbar';
 import FormText from './components/FormText';
 import About from './components/About';
+import { useState } from 'react';
 
 function App() {
+  const [mode, changeMode] = useState('light');
+
+  let changeTheme = () => {
+    if (mode === 'light') {
+      changeMode('dark');
+      document.body.style.backgroundColor='gray';
+    }
+    else {
+      changeMode('light');
+      document.body.style.backgroundColor='white';
+
+    }
+  }
+
   return (
     <>
-      <Navbar />
-      {/* <FormText heading='Enter the text below'/> */}
-      <About/>
+      <Navbar changeTheme={changeTheme} />
+      <FormText heading='Enter the text below' mode={mode} />
+      {/* <About/> */}
     </>
   );
 }
--- src/components/FormText.js
+++ src/components/FormText.js
@@ -13,6 +13,15 @@ export default function FormText(props) {
     function clearText() {
         setText('');
     }
+    function copyText() {
+        var textToCopy = document.getElementById('myBox');
+        textToCopy.select();
+        navigator.clipboard.writeText(text);
+    }
+    function removeExtraSpaces() {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(' '));
+    }
     function changeValue(e) {
         setText(e.target.value);
     }
@@ -30,17 +39,19 @@ export default function FormText(props) {
             <div className="container">
                 <h2 className="my-3">{props.heading}</h2>
                 <div className="mb-3">
-                    <textarea className="form-control" id="textArea" value={text} onChange={changeValue} rows="8" placeholder="Enter your text"></textarea>
+                    <textarea className="form-control" style={{ backgroundColor: props.mode === 'dark' ? '#212529' : 'white', color: props.mode === 'dark' ? '#f1f1f2' : 'black' }} id="myBox" value={text} onChange={changeValue} rows="8" placeholder="Enter your text"></textarea>
                 </div>
-                <button className="btn btn-primary" onClick={changeUpper}>Uppercase</button>
-                <button className="btn btn-primary mx-2" onClick={changeLower}>Lowercase</button>
+                <button className={`btn btn-${props.mode === 'dark' ? 'dark' : 'primary'}`} onClick={changeUpper}>Uppercase</button>
+                <button className={`btn btn-${props.mode === 'dark' ? 'dark' : 'primary'} mx-2`} onClick={changeLower}>Lowercase</button>
+                <button className={`btn btn-${props.mode === 'dark' ? 'dark' : 'primary'} mx-2`} onClick={copyText}>Copy</button>
+                <button className={`btn btn-${props.mode === 'dark' ? 'dark' : 'primary'} mx-2`} onClick={removeExtraSpaces}>Remove Extra Spaces</button>
                 <button className="btn btn-danger mx-2" onClick={clearText}>Clear</button>
             </div>
             <div className="container">
-                <h2 class="my-3">Your Text Summary</h2>
-                <p>{total_words} words {text.length} characters</p>
-                <p>{0.008 * total_words} Minutes read</p>
-            </div>
+                <h2 className="my-3">Your Text Summary</h2>
+                <p style={{fontWeight: 500, color: props.mode==='dark'?'white':'black'}}>{total_words} words {text.length} characters</p>
+            <p style={{fontWeight: 500, color: props.mode==='dark'?'white':'black'}}>{0.008 * total_words} Minutes read</p>
+        </div>
         </>
     )
 }
